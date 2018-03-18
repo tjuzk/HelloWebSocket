2cf5f3a9af3f71c8282aef8207c5c678b4c80e70
tjuzk <1098473085@qq.com>
2018-03-18 11:57:48 +0800

    Spring guide commit
    Spring guide demo: http://spring.io/guides/gs/messaging-stomp-websocket/#initial

--- ciny/src/main/java/com/websocket/ciny/Application.java
+++ ciny/src/main/java/com/websocket/ciny/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CinyApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CinyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
