6917b0f3cf8851359ac1c6230b3748162b061722
tjuzk <1098473085@qq.com>
2018-03-18 11:46:34 +0800

    Project initialization commit
    Project is created by Spring boot initializer.

---
+++ ciny/src/main/java/com/websocket/ciny/CinyApplication.java
@@ -0,0 +1,12 @@
+package com.websocket.ciny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinyApplication.class, args);
+	}
+}
---
+++ ciny/src/test/java/com/websocket/ciny/CinyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.websocket.ciny;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CinyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
